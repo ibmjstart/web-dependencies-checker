b3ff666c29b080c9a5396fe867465ca066e49109
Ryan Soley <resoley@us.ibm.com>
2016-11-07 15:57:17 -0500

    Perform http HEAD on urls

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 
+	"github.com/fatih/color"
 	"gopkg.in/yaml.v2"
 )
 
@@ -11,12 +13,18 @@ var data = `
 services:
  - name: firstService
    sites:
-    - google.com
-    - github.com
+    - http://github.com
+    - http://google.com
  - name: secondService
    sites:
-    - google.com
+    - http://google.com
+    - https://w3-03.sso.ibm.com/
 `
+
+var white (func(string, ...interface{}) string) = color.New(color.FgHiWhite, color.Bold).SprintfFunc()
+var green (func(string, ...interface{}) string) = color.New(color.FgGreen, color.Bold).SprintfFunc()
+var red (func(string, ...interface{}) string) = color.New(color.FgRed, color.Bold).SprintfFunc()
+var blue (func(string, ...interface{}) string) = color.New(color.FgBlue, color.Bold).SprintfFunc()
 
 type service struct {
 	Name  string
@@ -25,17 +33,45 @@ type service struct {
 
 type serviceList struct {
 	Services []service
+	statuses map[string]string
+}
+
+func ServiceList(source []byte) (*serviceList, error) {
+	var services serviceList
+
+	services.statuses = make(map[string]string)
+	err := yaml.Unmarshal(source, &services)
+	if err != nil {
+		return nil, err
+	}
+
+	return &services, nil
+}
+
+func (s *serviceList) testService(service *service) {
+	for _, url := range service.Sites {
+		if _, found := s.statuses[url]; !found {
+			response, err := http.Head(url)
+			if err != nil {
+				s.statuses[url] = err.Error()
+			} else {
+				s.statuses[url] = response.Status
+			}
+		}
+	}
 }
 
 func main() {
-	var services serviceList
-
 	source := []byte(data)
-
-	err := yaml.Unmarshal(source, &services)
+	services, err := ServiceList(source)
 	if err != nil {
 		os.Exit(1)
 	}
 
 	fmt.Printf("%v\n", services)
+	for _, cur := range services.Services {
+		services.testService(&cur)
+	}
+
+	fmt.Printf("%v\n", services.statuses)
 }
