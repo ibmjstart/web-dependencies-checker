9f0fdff98e5ef9c520a5c24269509fa0f9cec81a
Ryan Soley <resoley@us.ibm.com>
2016-11-07 10:55:17 -0500

    Basic YAML parsing

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+var data = `
+services:
+ - name: firstService
+   sites:
+    - google.com
+    - github.com
+ - name: secondService
+   sites:
+    - google.com
+`
+
+type service struct {
+	Name  string
+	Sites []string
+}
+
+type serviceList struct {
+	Services []service
+}
 
 func main() {
-	fmt.Println("vim-go")
+	var services serviceList
+
+	source := []byte(data)
+
+	err := yaml.Unmarshal(source, &services)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	fmt.Printf("%v\n", services)
 }
